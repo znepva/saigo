66227917f052a57d22afdc9ff6d043c63798a21f
Marcin Marjanski <mmarjanski@enova.com>
2016-09-13 13:22:39 -0500

    make hash safe for concurrent use

--- exercise-003-web/solution/server.go
+++ exercise-003-web/solution/server.go
@@ -3,12 +3,18 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"sync"
 )
 
 var homeT *template.Template
 
-var userData = make(map[string]int)
+// make map concurrency safe
+var counter = struct{
+	sync.RWMutex
+	m map[string]int
+}{m: make(map[string]int)}
 
+// render home page
 func home(w http.ResponseWriter, r *http.Request) {
 	err := homeT.Execute(w, nil)
 	if err != nil {
@@ -16,26 +22,38 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// process post request
 func signup(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.Form.Get("username")
-	userData[username]++
 
-	err := homeT.Execute(w, &userData)
+	// update user counter
+	counter.Lock()
+	counter.m[username]++
+	counter.Unlock()
+
+	// render view
+	counter.RLock()
+	err := homeT.Execute(w, &counter.m)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	counter.RUnlock()
 }
 
 func main() {
+	// initialize home template
 	var err error
 	homeT = template.Must(template.ParseFiles("solution/home.html"))
 	if err != nil {
 		panic(err)
 	}
 
+	// routes
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/signup", signup)
+
+	// start server
 	http.ListenAndServe(":8080", nil)
 }
 
